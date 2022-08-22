8daecc86bee10eaecbc2323e4d6c7dea8722d669
Ulathi <swiftly.graphics@gmail.com>
2022-08-22 13:55:07 +0200

    simple api v1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
-app.use('/user', (req, res, next) => {
+app.use('/api', (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
